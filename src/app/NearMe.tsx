66f7ddee39f4b9d08317e0da23b7d508da730b45
Cameron Kleiman <camtheman256@gmail.com>
2023-06-20 22:10:12 -0700

    improve loading

--- src/app/NearMe.tsx
+++ src/app/NearMe.tsx
@@ -9,6 +9,7 @@ import { Badge, Col, ListGroup, Row, Spinner } from "react-bootstrap";
 export default function NearMe() {
   const [location, setLocation] = useState<GeolocationPosition>();
   const [stops, setStops] = useState<NearMeStop[]>([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((p) => setLocation(p), null, {
@@ -22,7 +23,8 @@ export default function NearMe() {
       `/api/stops?lat=${location.coords.latitude}&lon=${location.coords.longitude}`
     )
       .then((r) => r.json())
-      .then((d) => setStops(d));
+      .then((d) => setStops(d))
+      .then(() => setLoaded(true));
   }, [location]);
 
   if (!location) return <LocationError />;
@@ -30,9 +32,15 @@ export default function NearMe() {
   return (
     <>
       <h1>Nearby Stops</h1>
+      {loaded ? 
       <ListGroup>
-        {stops.map((s, i) => <Stop key={i} stop={s} />)}
-      </ListGroup>
+        {stops.map((s, i) => (
+          <Stop key={i} stop={s} />
+        ))}
+      </ListGroup> : <p>
+        <Spinner /> Loading stops near you
+      </p>
+      }
     </>
   );
 }
@@ -60,18 +68,24 @@ function Stop(props: { stop: NearMeStop }) {
           <h5>{props.stop.stop.stop_name}</h5>
           <small className="text-muted">
             {Math.round(props.stop.distAway)}m
-          </small> <br />
+          </small>{" "}
+          <br />
           <small>
             Running every{" "}
-            {departures && <b>{findFrequency(departures.departures ?? [])}</b>} mins
+            {departures && <b>{findFrequency(departures.departures ?? [])}</b>}{" "}
+            mins
           </small>
         </Col>
         <Col>
           <Row>
-            {departures ? ( departures.departures?.length ? 
-              departures.departures?.map((d, i) => (
-                <Departure data={d} key={i} />
-              )) : <p className="text-muted">No departures.</p>
+            {departures ? (
+              departures.departures?.length ? (
+                departures.departures?.map((d, i) => (
+                  <Departure data={d} key={i} />
+                ))
+              ) : (
+                <p className="text-muted">No departures.</p>
+              )
             ) : (
               <Spinner />
             )}
@@ -101,19 +115,25 @@ function Departure(props: { data: TransitLandDeparture }) {
       >
         {props.data.trip.route.route_short_name}
       </Badge>
-      <h5 className={props.data.departure.estimated_utc ? '' : 'text-muted'}>{minsRemaining}</h5>
+      <h5 className={props.data.departure.estimated_utc ? "" : "text-muted"}>
+        {minsRemaining}
+      </h5>
       <small>{props.data.trip.trip_headsign}</small>
     </Col>
   );
 }
 
 function findFrequency(departures: TransitLandDeparture[]) {
-  const estimatedDepartures = departures.filter(d => d.departure.estimated_utc !== null)
+  const estimatedDepartures = departures.filter(
+    (d) => d.departure.estimated_utc !== null
+  );
   let gap = 0;
   for (let i = 1; i < estimatedDepartures.length; i++) {
     gap +=
-      new Date(estimatedDepartures[i].departure.estimated_utc ?? '').getTime() -
-      new Date(estimatedDepartures[i - 1].departure.estimated_utc ?? '').getTime();
+      new Date(estimatedDepartures[i].departure.estimated_utc ?? "").getTime() -
+      new Date(
+        estimatedDepartures[i - 1].departure.estimated_utc ?? ""
+      ).getTime();
   }
   return Math.round(gap / 60000 / (estimatedDepartures.length - 1));
 }
